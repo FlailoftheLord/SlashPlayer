80f141e1210b48c6e8df06ba89293f31b5a16b3a
FlailoftheLord <jeddyh15@gmail.com>
2019-05-28 20:32:40 -0400

    Update FreezeListener.java

--- src/me/flail/slashplayer/listeners/FreezeListener.java
+++ src/me/flail/slashplayer/listeners/FreezeListener.java
@@ -1,5 +1,24 @@
 package me.flail.slashplayer.listeners;
 
-public class FreezeListener {
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.AsyncPlayerChatEvent;
+import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.event.player.PlayerMoveEvent;
+
+import me.flail.slashplayer.sp.Message;
+import me.flail.slashplayer.user.User;
+
+public class FreezeListener implements Listener {
+
+	@EventHandler(priority = EventPriority.HIGHEST)
+	public void freezeListen(PlayerMoveEvent moveEvent, PlayerInteractEvent interactEvent, AsyncPlayerChatEvent chatEvent) {
+		User subject;
+		Message freezeMove = new Message("FreezeMove");
+		Message freezeInteract = new Message("FreezeInteract");
+		Message freezeOther = new Message("FreezeOther");
+
+	}
 
 }
